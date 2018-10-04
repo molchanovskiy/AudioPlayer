9ecf89c158dda32bc6a84c6ca3471eec06c402d5
Andrey Molchanovskiy <a.molchanovskiy@gmail.com>
2018-10-04 20:50:03 +0300

    added fixes

--- AudioPlayer/AudioPlayer/item/AudioItemQueue.swift
+++ AudioPlayer/AudioPlayer/item/AudioItemQueue.swift
@@ -180,8 +180,11 @@ class AudioItemQueue {
         }
 
         while nextPosition > 0 {
-            let previousPosition = nextPosition - 1
+            var previousPosition = nextPosition - 1
             nextPosition = previousPosition
+            if previousPosition == queue.count - 1 && mode == .normal {
+                previousPosition -= 1
+            }
             let item = queue[previousPosition]
 
             if shouldConsiderItem(item: item) {
@@ -199,7 +202,7 @@ class AudioItemQueue {
     /// A boolean value indicating whether the queue has a previous item to play or not.
     var hasPreviousItem: Bool {
         if !queue.isEmpty &&
-            (nextPosition > 0 || mode.contains(.repeat) || mode.contains(.repeatAll)) {
+            (nextPosition > 1 || mode.contains(.repeat) || mode.contains(.repeatAll)) {
             return true
         }
         return false
